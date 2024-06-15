6bf62c8122ac17e9f87e8c7218392e8c540297b1
HysenB <hysi2000@live.com>
2024-06-15 17:05:31 +0200

    bug fix

--- database/migrations/2024_06_14_183545_create_files_table.php
+++ database/migrations/2024_06_14_183545_create_files_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('mime')->nullable();
             $table->integer('size')->nullable();
             $table->timestamps();
-            $table->foreignIdFor(\app\Models\User::class, 'created_by');
+            $table->foreignIdFor(\App\Models\User::class, 'created_by');
             $table->foreignIdFor(\App\Models\User::class, '
             updated_by');
             $table->softDeletes();
